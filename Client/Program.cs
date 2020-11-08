90f3898b23b6dfbe929949036ce7b184764ac62b
Костя Жихарев <kostyazhikharev@gmail.com>
2020-11-08 13:59:57 +0300

    Add while to client

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,7 +11,7 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Connect("127.0.0.1",Console.ReadLine());
+            Connect("127.0.0.1","0");
         }
         static void Connect(String server, String message)
         {
@@ -23,32 +23,45 @@ namespace Client
                 // combination.
                 Int32 port = 13000;
                 TcpClient client = new TcpClient(server, port);
-
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
-
                 NetworkStream stream = client.GetStream();
+                
+                while(true)
+                {
+                    message = Console.ReadLine();
+                    // Translate the passed message into ASCII and store it as a Byte array.
+                    if(message != null)
+                    {
+                        Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
 
-                // Send the message to the connected TcpServer.
-                stream.Write(data, 0, data.Length);
+                    // Get a client stream for reading and writing.
+                    //  Stream stream = client.GetStream();
 
-                Console.WriteLine("Sent: {0}", message);
+                    
+                    // Send the message to the connected TcpServer.
+                     stream.Write(data, 0, data.Length);
 
-                // Receive the TcpServer.response.
+                     Console.WriteLine("Sent: {0}", message);
 
-                // Buffer to store the response bytes.
-                data = new Byte[256];
+                     // Receive the TcpServer.response.
 
-                // String to store the response ASCII representation.
-                String responseData = String.Empty;
+                     // Buffer to store the response bytes.
+                    data = new Byte[256];
 
-                // Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);
+                    // String to store the response ASCII representation.
+                    String responseData = String.Empty;
+
+                    // Read the first batch of the TcpServer response bytes.
+                    Int32 bytes = stream.Read(data, 0, data.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                    Console.WriteLine("Received: {0}", responseData);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    
+                }
+                
 
                 // Close everything.
                stream.Close();
